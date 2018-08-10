a44497470681fc3fcb8f9e9b3ceaecc989e521fa
root <root@localhost.localdomain>
2018-08-10 13:16:07 +0200

    Install

--- install.bash
+++ install.bash
@@ -2,14 +2,22 @@
 
 GLOBAL_FOLDER_SCRIPT=$(/usr/bin/dirname "$0")
 
+if [ ${GLOBAL_FOLDER_SCRIPT:0:1} == "." ]
+then
+ GLOBAL_FOLDER_SCRIPT="$(/bin/pwd)${GLOBAL_FOLDER_SCRIPT:1}"
+elif [ ${GLOBAL_FOLDER_SCRIPT:0:1} != "/" ]
+then
+ GLOBAL_FOLDER_SCRIPT="$(/bin/pwd)/$GLOBAL_FOLDER_SCRIPT"
+fi
+
 read -e -p "Local image file: " -i '/usr/share/backgrounds/national.geographic-photo.of.the.day.jpg' GLOBAL_FILE_IMAGE
 
-/bin/mkdir etc
+/bin/mkdir "$GLOBAL_FOLDER_SCRIPT/etc"
 
 echo "#!/bin/bash
 
 export CONFIG_FILE_IMAGE=\"$GLOBAL_FILE_IMAGE\"
-" 1>"etc/kbcom.net-natgeopod-downloader.bash.conf"
+" 1>"$GLOBAL_FOLDER_SCRIPT/etc/kbcom.net-natgeopod-downloader.bash.conf"
 
 echo "#!/bin/bash
 
@@ -18,9 +26,9 @@ CONFIG_FOLDER_MAIN=\"$GLOBAL_FOLDER_SCRIPT\"
 source \"\$CONFIG_FOLDER_MAIN/etc/kbcom.net-natgeopod-downloader.bash.conf\"
 
 \"\$CONFIG_FOLDER_MAIN/usr/share/kbcom.net-natgeopod-downloader.bash\"
-" 1>"run.bash"
+" 1>"$GLOBAL_FOLDER_SCRIPT/run.bash"
 
-/bin/chmod a+x "run.bash"
+/bin/chmod a+x "$GLOBAL_FOLDER_SCRIPT/run.bash"
 
 echo "[Unit]
 Description=National Geographic - photo of the day downloader
@@ -34,8 +42,8 @@ ExecStart=$GLOBAL_FOLDER_SCRIPT/run.bash
 
 [Install]
 WantedBy=multi-user.target
-" 1>"local/etc/systemd/natgeopod-downloader.service"
+" 1>"$GLOBAL_FOLDER_SCRIPT/local/etc/systemd/natgeopod-downloader.service"
 
-/bin/ln -s $GLOBAL_FOLDER_SCRIPT/local/etc/systemd/natgeopod-downloader.service /lib/systemd/system
+/bin/ln -sf "$GLOBAL_FOLDER_SCRIPT/local/etc/systemd/natgeopod-downloader.*" "/lib/systemd/system"
 
 /bin/systemctl enable --now natgeopod-downloader.timer
