24d47db8f6994b83c8aaedfdc91e8151f047110a
root <root@localhost.localdomain>
2018-08-10 12:31:19 +0200

    Install

--- install.bash
+++ install.bash
@@ -15,7 +15,7 @@ CONFIG_FOLDER_MAIN=\"$(/bin/pwd)\"
 
 source \"\$CONFIG_FOLDER_MAIN/etc/kbcom.net-natgeopod-downloader.bash.conf\"
 
-\"\$CONFIG_FOLDER_MAIN/usr/share/kbcom.net-natgeopod-downloader.bash.bash\"
+\"\$CONFIG_FOLDER_MAIN/usr/share/kbcom.net-natgeopod-downloader.bash\"
 " 1>"run.bash"
 
 /bin/chmod a+x "run.bash"
